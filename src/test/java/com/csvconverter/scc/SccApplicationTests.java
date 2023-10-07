32790b27cffbd25a4d8d077a631dac065543672b
schikimikiviki <viktoria.bzdyk@gmx.at>
2023-10-07 09:12:31 +0200

    initial commit

---
+++ src/test/java/com/csvconverter/scc/SccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csvconverter.scc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
